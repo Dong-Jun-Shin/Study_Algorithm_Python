7da10fb5de24848023100551d59d8c0a1f0d1e5a
Dongjun-Shin <tlsehdwns239@gmail.com>
2021-06-25 15:51:27 +0900

    Chapter2 - 소수 나열
    모든 수 나누기

---
+++ example_basic/chap02/prime1.py
@@ -0,0 +1,14 @@
+# 1,000이하의 소수를 나열하기
+# for~else문을 이용한 loop out과 loop end 처리하기
+
+
+counter = 0         # 나눗셈 횟수를 카운트
+
+for n in range(2, 1001):
+    for i in range(2, n):
+        counter += 1
+        if n % i == 0:  # 나누어 떨어지면 소수가 아님
+            break       # 반복은 더 이상 불필요하여 중단
+    else:               # 끝까지 나누어 떨어지지 않으면 다음을 수행
+        print(n)
+print(f'나눗셈을 실행한 횟수: {counter}')
