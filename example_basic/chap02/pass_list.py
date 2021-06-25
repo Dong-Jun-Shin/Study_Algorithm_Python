5873effd51494e03d92972ca064cab9a04ec5eba
Dongjun-Shin <tlsehdwns239@gmail.com>
2021-06-25 15:50:34 +0900

    Chapter2 - 매개변수 전달
    매개변수 전달 시, mutable 객체를 참조하는 변수의 경우

---
+++ example_basic/chap02/pass_list.py
@@ -0,0 +1,17 @@
+# 리스트에서 임의의 원솟값을 업데이트하기
+# 매개변수 전달 시, mutable 객체를 참조하는 변수의 경우, 참조하는 객체를 변경 (list 등등)
+
+
+def change(lst, idx, val):
+    """lst[idx]의 값을 val로 업데이트"""
+    lst[idx] = val
+
+
+x = [11, 22, 33, 44, 55]
+print('x = ', x)
+
+index = int(input('업데이트할 인덱스를 선택하세요.: '))
+value = int(input('새로운 값을 입력하세요.: '))
+
+change(x, index, value)
+print(f'x = {x}')
