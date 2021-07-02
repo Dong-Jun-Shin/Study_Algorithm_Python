c780fe9ffc8d02f00d5b1ca4fb8ed8ab96010a96
Dongjun-Shin <tlsehdwns239@gmail.com>
2021-07-02 16:16:42 +0900

    Chapter4 - 스택
    오타 수정

--- example_basic/chap04/fixed_stack.py
+++ example_basic/chap04/fixed_stack.py
@@ -63,7 +63,7 @@ class FixedStack:
                 return i                            # 검색 성공
         return -1                                   # 검색 실패
 
-    def count(self, value: Any) -> bool:
+    def count(self, value: Any) -> int:
         """스택에 있는 value의 개수를 반환"""
         c = 0
         for i in range(self.ptr):                   # 바닥 쪽부터 선형 검색
@@ -73,7 +73,7 @@ class FixedStack:
 
     def __contains__(self, value: Any) -> bool:
         """스택에 value가 있는지 판단"""
-        return self.count(value)
+        return self.count(value) > 0
 
     def dump(self) -> None:
         """덤프(스택 안의 모든 데이터를 바닥부터 꼭대기 순으로 출력)"""
