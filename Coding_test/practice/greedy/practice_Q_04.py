64fe45265ab961d4f0642062f3ee49baabd1ceb3
Dongjun-Shin <tlsehdwns239@gmail.com>
2021-07-23 19:01:22 +0900

    Update practice_Q_04.py
    주석, 풀이 추가

--- Coding_test/practice/greedy/practice_Q_04.py
+++ Coding_test/practice/greedy/practice_Q_04.py
@@ -2,6 +2,21 @@
 n = int(input())
 unitList = list(map(int, input().split()))
 
+"""-------------------------book
+# 작은 단위부터 더해서, 해당 값까지의 수는 만들어진다.
+# 하지만 다음 단위가 더 클 경우, 현재 target값은 만들 수 없게 된다.
+# (target=12, +unit:13 -> 12를 만들 수 없다)
+unitList.sort()
+target = 1
+for unit in unitList:
+    if target < unit:
+        break
+    target += unit
+"""
+# ----------------------------me
+# 큰 단위부터 현재 i에서 뺀다.
+# 단위를 다 돌렸을 때, 0이 되면 만들 수 있는 수이다.
+# 0이 나오지 않을 때까지 계산한다.
 unitList.sort(reverse=True)
 for i in range(1, 1000000):
     num = i
@@ -10,5 +25,6 @@ for i in range(1, 1000000):
             num -= unit
     if not (num == 0):
         break
+# ------------------------------
 
 print(i)
