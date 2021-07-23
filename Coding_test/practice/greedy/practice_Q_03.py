01e448261d445a4604b032426cfc50b41376b76e
Dongjun-Shin <tlsehdwns239@gmail.com>
2021-07-23 18:45:02 +0900

    Update practice_Q_03.py
    주석 추가

--- Coding_test/practice/greedy/practice_Q_03.py
+++ Coding_test/practice/greedy/practice_Q_03.py
@@ -3,6 +3,7 @@ strList = list(map(int, input()))
 
 count = 0
 for i in range(1, len(strList)):
+    # 현재 문자열과 시작 문자열이 다르고 이전 문자열과도 다르면 카운트
     if strList[i] != strList[0] and strList[i] != strList[i - 1]:
         count += 1
 
