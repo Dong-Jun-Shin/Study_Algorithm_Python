bb9696ef9c23d9905fab182a9b07d4ea70cd71a4
Dongjun-Shin <tlsehdwns239@gmail.com>
2021-09-07 13:18:49 +0900

    Part3_Turn2 - 그래프 복습
    그래프 연습문제

---
+++ Coding_test/practice_turn_2/08_graph/prac_turn2_10_4.py
@@ -0,0 +1,42 @@
+# 커리큘럼
+from collections import deque
+import copy
+
+
+def topology_sort():
+    min_cost = copy.deepcopy(cost)
+    q = deque()
+    for i in range(1, n + 1):
+        if indegrees[i] == 0:
+            q.append(i)
+    while q:
+        node_idx = q.popleft()
+        for node in graph[node_idx]:
+            min_cost[node] = max(min_cost[node], min_cost[node_idx] + cost[node])
+            indegrees[node] -= 1
+            if indegrees[node] == 0:
+                q.append(node)
+    return min_cost
+
+n = int(input())
+indegrees = [0] * (n + 1)
+graph = [[] for _ in range(n + 1)]
+cost = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    datas = list(map(int, input().split()))
+    cost[i] = datas[0]
+    for data in datas[1:-1]:
+        indegrees[i] += 1
+        graph[data].append(i)
+
+print(topology_sort())
+
+""" Test case
+5
+10 -1
+10 1 -1
+4 1 -1
+4 3 1 -1
+3 3 -1
+"""
