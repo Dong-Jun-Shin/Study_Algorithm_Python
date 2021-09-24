d1b7e60fcea9cb5d32ca77e76cc5c456ac33ea99
Dongjun-Shin <tlsehdwns239@gmail.com>
2021-09-24 11:15:10 +0900

    Part3_Turn3 - 그래프 복습
    기출문제 복습
    4회차 복습할 문제
    - 탑승구

---
+++ Coding_test/practice_turn_3/08_graph/prac_turn3_Q_42.py
@@ -0,0 +1,28 @@
+# 탑승구
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a):
+    a = find_parent(parent, a)
+    b = a - 1
+    if a > b:
+        parent[a] = b
+    else:
+        parent[b] = a
+
+
+g = int(input())
+p = int(input())
+count = 0
+parent = [i for i in range(g + 1)]
+for _ in range(p):
+    p_num = int(input())
+    if find_parent(parent, p_num) == 0:
+        break
+    union_parent(parent, p_num)
+    count += 1
+
+print(count)
