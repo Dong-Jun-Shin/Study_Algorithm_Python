818396f32d42420660cecf7d372eafb1d7630172
Dongjun-Shin <tlsehdwns239@gmail.com>
2021-10-20 20:14:21 +0900

    programmers 문제 - level2
    조이스틱

---
+++ programmers/level2/c30_42860.py
@@ -0,0 +1,23 @@
+# 조이스틱
+def solution(name):
+    answer = 0
+    n = len(name)
+
+    def alphabet_to_num(char):
+        num_char = [i for i in range(14)] + [j for j in range(12, 0, -1)]
+        return num_char[ord(char) - ord('A')]
+
+    for ch in name:
+        answer += alphabet_to_num(ch)
+
+    move = n - 1
+    for idx in range(n):
+        next_idx = idx + 1
+        while (next_idx < n) and (name[next_idx] == 'A'):
+            next_idx += 1
+        distance = min(idx, n - next_idx)
+        # idx: 0~A_S_idx, n - next_idx: A_E_idx, distance: 왼쪽 오른쪽 중 짧은 거리 방향으로 시작
+        move = min(move, idx + n - next_idx + distance)
+
+    answer += move
+    return answer
